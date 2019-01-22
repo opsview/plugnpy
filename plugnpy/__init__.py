63113a46cc9fac717f8f5a2865e5e4cafb79da63
nicolaasuni <nicola.asuni@opsview.com>
2019-01-22 11:46:49 +0000

    Add versioning and description

--- plugnpy/__init__.py
+++ plugnpy/__init__.py
@@ -1,3 +1,9 @@
+"""plugnpy - A Simple Python Library for creating Opsview Opspack plugins"""
+
+__version__ = '1.0.0'
+__release__ = '1'
+__program_name__ = 'plugnpy'
+
 from .check import Check
 from .exception import ParamError, ResultError, AssumedOK, InvalidMetricThreshold, InvalidMetricName
 from .parser import Parser
